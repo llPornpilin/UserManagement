f3ec5eff60c352069fbeb170d478aba1444ae0b6
llPornpilin <linglingppl@gmail.com>
2024-03-17 17:11:17 +0700

    Initial + Side navbar

---
+++ frontend/userManagementFront/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { TopbarComponent } from './core/components/topbar/topbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    TopbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
